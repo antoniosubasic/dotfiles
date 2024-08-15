0417b44291e7dd8050d4cf9515d03b5f49339be1
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-08-15 18:47:54 +0200

    restructured aliases.sh - with comments

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -1,6 +1,13 @@
+# ripgrep
 alias grep='rg'
+
+# bat
+alias bat='batcat'
+
+# xsel
 alias copy='xsel --input --clipboard'
 alias paste='xsel --output --clipboard'
+
+# cd
 alias ..='cd ..'
 alias ...='cd ../..'
-alias bat='batcat'
