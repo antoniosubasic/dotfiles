88e56e41807922ef54aaf326a101cea13f74f487
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-08-15 19:12:26 +0200

    added package eza to + automated installation + aliases

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -4,6 +4,11 @@ alias grep='rg'
 # bat
 alias bat='batcat'
 
+# eza - https://github.com/eza-community/eza?tab=readme-ov-file#display-options
+alias ls='eza --time-style=+"%d %b %Y %H:%M:%S" --color=always'
+alias ll='ls -al'
+alias tree='ls -T'
+
 # xsel
 alias copy='xsel --input --clipboard'
 alias paste='xsel --output --clipboard'
