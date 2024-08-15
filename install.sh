6675d764eef774fe265c17638a1d8d00e695c58f
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-08-15 21:14:54 +0200

    updated entire install.sh to have detailed & consistent logging

--- install.sh
+++ install.sh
@@ -1,35 +1,23 @@
 #!/bin/bash
 
-# colors
-CYAN='\033[0;36m'
-GREEN='\033[0;32m'
+# ---------------------------------- colors ----------------------------------
 RED='\033[0;31m'
+GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
 NC='\033[0m'
+# ---------------------------------- colors ----------------------------------
 
 # dotfiles path
 dotfiles_path="$HOME/.dotfiles"
 
-# option to force symlink creation
+# force symlink creation
 force=false
 if [[ $1 == "-f" || $1 == "--force" ]]; then
     force=true
 fi
-
-
-print() {
-    local message=$1
-    local color=$2
-    local delete=$3
-    echo -ne "${delete:+\n}\r${color}${message}${NC}\033[K"
-}
-
-errorif() {
-    if [[ $1 -ne 0 ]]; then
-        echo -e "${RED}$2${NC}"
-        exit 1
-    fi
-}
 
 install() {
     local package=$1
@@ -41,10 +29,11 @@ install() {
         *) echo -e "${RED}invalid package manager${NC}"; exit 1 ;;
     esac
 
-    print "installing $package" $YELLOW true
     $command $package > /dev/null 2>&1
-    errorif $? "failed to install $package"
-    print "$package installed" $CYAN
+    if [[ $? -ne 0 ]]; then
+        echo -e "${RED}failed to install ${CYAN}$package${NC}"
+        exit 1
+    fi
 }
 
 symlink() {
@@ -60,23 +49,23 @@ symlink() {
             shopt -s dotglob
             if [[ ! -d $base_link ]]; then
                 mkdir -p $base_link
-                echo -e "${CYAN}$base_link${NC} created"
+                echo -e "\t${CYAN}$base_link${NC} created"
             fi
             for item in $base_target/*; do
                 target="$item"
                 link="$base_link/$(basename $item)"
                 if [[ -e $link && $force == false ]]; then
-                    echo -e "${YELLOW}$(basename $link)${NC} already exists"
+                    echo -e "\t${YELLOW}$(basename $link)${NC} already exists"
                 elif [[ -e $target ]]; then
                     ln -sf $target $link
-                    echo -e "${CYAN}$link${NC} -> ${GREEN}$target${NC}"
+                    echo -e "\t${CYAN}$link${NC} -> ${GREEN}$target${NC}"
                 else
-                    echo -e "${YELLOW}$target${NC} not found"
+                    echo -e "\t${YELLOW}$target${NC} not found"
                 fi
             done
             shopt -u dotglob
         else
-            echo -e "${YELLOW}$dir${NC} not found"
+            echo -e "\t${YELLOW}$dir${NC} not found"
         fi
 
     # handle single file or directory
@@ -87,17 +76,44 @@ symlink() {
         fi
 
         if [[ -e $link && $force == false ]]; then
-            echo -e "${YELLOW}$(basename $link)${NC} already exists"
+            echo -e "\t${YELLOW}$(basename $link)${NC} already exists"
         else
             ln $link_options "$target" "$link"
-            echo -e "${CYAN}$link${NC} -> ${GREEN}$target${NC}"
+            echo -e "\t${CYAN}$link${NC} -> ${GREEN}$target${NC}"
         fi
 
     # handle invalid
     else
-        echo -e "${YELLOW}$target${NC} not found"
+        echo -e "\t${YELLOW}$target${NC} not found"
     fi
 }
+
+# ---------------------------------- section handling ----------------------------------
+section=""
+
+sectionlog() {
+    if [[ $2 == "start" ]]; then
+        section=$1
+        echo -ne "---------------------------------------------------------------------------\n[${YELLOW}IN${NC}] $section"
+    elif [[ $2 == "end" ]]; then
+        echo ""
+
+        if [[ $3 == true ]]; then
+            echo -e "[${GREEN}OK${NC}] $section"
+        else
+            echo -e "[${RED}ER${NC}] $section"
+        fi
+
+        echo -e "---------------------------------------------------------------------------"
+    else
+        if [[ $2 == true ]]; then
+            echo -ne "\r\t$1\033[K"
+        else
+            echo -ne "\n\t$1\033[K"
+        fi
+    fi
+}
+# ---------------------------------- section handling ----------------------------------
 
 if [[ $EUID -eq 0 ]]; then
     echo "must not be run as root" 
@@ -106,59 +122,116 @@ fi
 
 sudo -v
 
-# update repositories
-print "updating repositories" $YELLOW
-sudo apt-get update > /dev/null 2>&1
-errorif $? "failed to update repositories"
-print "repositories updated" $CYAN
+# ---------------------------------- system ----------------------------------
+sectionlog "system" "start"
 
-# install apt packages
-packages=(build-essential libssl-dev curl git xsel ripgrep bat)
+# update system
+sectionlog "updating ${CYAN}system${NC}"
+sudo apt-get update > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    sectionlog "${RED}failed to update ${CYAN}system${NC}" true
+    exit 1
+else
+    sectionlog "${CYAN}system${NC} updated" true
+fi
+
+# install essential packages
+packages=(build-essential libssl-dev curl wget)
 for package in ${packages[@]}; do
+    sectionlog "installing ${CYAN}$package${NC}"
     install $package apt
+    sectionlog "${CYAN}$package${NC} installed" true
 done
 
 # upgrade system
-print "upgrading system" $YELLOW true
+sectionlog "upgrading ${CYAN}system${NC}"
 sudo apt-get upgrade -y > /dev/null 2>&1
-errorif $? "failed to upgrade system"
-print "system upgraded" $CYAN
+if [[ $? -ne 0 ]]; then
+    sectionlog "${RED}failed to upgrade ${CYAN}system${NC}" true
+    exit 1
+else
+    sectionlog "${CYAN}system${NC} upgraded" true
+fi
 
-# install latest dotnet
-print "installing dotnet" $YELLOW true
+sectionlog "system" "end" true
+# ---------------------------------- system ----------------------------------
+
+# ---------------------------------- languages ----------------------------------
+sectionlog "languages" "start"
+
+# install dotnet
+sectionlog "installing ${CYAN}dotnet${NC}"
 release_index=$(eval curl -s https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json)
 dotnet_versions=$(echo "$release_index" | grep -Eo '"latest-release": "[0-9]\.[0-9]+\.[0-9]+"' | awk -F '"' '{print $4}')
 dotnet_version=$(echo "$dotnet_versions" | sort -r -t '"' -k4,4n | head -n 1 | awk -F '.' '{print $1"."$2}')
 sudo apt-get install dotnet-sdk-$dotnet_version > /dev/null 2>&1
-errorif $? "failed to install dotnet"
-print "dotnet $dotnet_version installed" $CYAN
-
-# install latest rust
-print "installing rust" $YELLOW true
-curl https://sh.rustup.rs -sSf | sh -s -- -y >/dev/null 2>&1
-errorif $? "failed to install rust"
-source $HOME/.cargo/env
-source $HOME/.profile
-source $HOME/.bashrc
-print "rust installed" $CYAN
-
-# install eza
-install eza cargo
-
-# update dotfiles repository
-if [[ -d $dotfiles_path ]]; then
-    print "pulling dotfiles" $YELLOW true
-    git -C $dotfiles_path pull > /dev/null 2>&1
-    errorif $? "failed to pull dotfiles repository"
-    print "dotfiles pulled" $CYAN
+if [[ $? -ne 0 ]]; then
+    sectionlog "${RED}failed to install ${CYAN}dotnet${NC}" true
 else
-    print "cloning dotfiles" $YELLOW true
-    git clone https://github.com/antoniosubasic/dotfiles.git $dotfiles_path > /dev/null 2>&1
-    errorif $? "failed to clone dotfiles repository"
-    print "dotfiles cloned" $CYAN
+    sectionlog "${CYAN}dotnet $dotnet_version${NC} installed" true
 fi
 
-echo -e "\n\n${GREEN}creating symlinks:${NC}"
+# install latest rust
+sectionlog "installing ${CYAN}rust${NC}"
+curl https://sh.rustup.rs -sSf | sh -s -- -y >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    sectionlog "${RED}failed to install ${CYAN}rust${NC}"
+    exit 1
+else
+    source $HOME/.cargo/env
+    source $HOME/.profile
+    source $HOME/.bashrc
+    sectionlog "${CYAN}rust${NC} installed" true
+fi
+
+sectionlog "languages" "end" true
+# ---------------------------------- languages ----------------------------------
+
+# ---------------------------------- packages ----------------------------------
+sectionlog "packages" "start"
+
+packages=(git xsel ripgrep bat)
+for package in ${packages[@]}; do
+    sectionlog "installing ${CYAN}$package${NC}"
+    install $package apt
+    sectionlog "${CYAN}$package${NC} installed" true
+done
+
+sectionlog "installing ${CYAN}eza${NC}"
+install eza cargo
+sectionlog "${CYAN}eza${NC} installed" true
+
+sectionlog "packages" "end" true
+# ---------------------------------- packages ----------------------------------
+
+# ---------------------------------- dotfiles repo ----------------------------------
+sectionlog "dotfiles" "start"
+
+if [[ -d $dotfiles_path ]]; then
+    sectionlog "pulling ${CYAN}repo${NC}"
+    git -C $dotfiles_path pull > /dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        sectionlog "${RED}failed to pull ${CYAN}repo${NC}" true
+    else
+        sectionlog "${CYAN}repo${NC} pulled" true
+    fi
+else
+    sectionlog "cloning ${CYAN}repo${NC}"
+    git clone https://github.com/antoniosubasic/dotfiles.git $dotfiles_path > /dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        sectionlog "${RED}failed to clone ${CYAN}repo${NC}" true
+    else
+        sectionlog "${CYAN}repo${NC} cloned" true
+    fi
+fi
+
+sectionlog "dotfiles" "end" true
+# ---------------------------------- dotfiles repo ----------------------------------
+
+# ---------------------------------- symlinks ----------------------------------
+sectionlog "symlinks" "start"
+
+echo ""
 
 # create symlinks for dotfiles
 dotfiles=(.gitconfig .config/*)
@@ -170,7 +243,10 @@ done
 if ! grep -q "# custom dotfiles configuration" "$HOME/.bashrc"; then
     echo -e "\n# custom dotfiles configuration" >> "$HOME/.bashrc"
     echo "source ~/.config/bash/init.sh" >> "$HOME/.bashrc"
-    echo -e "${CYAN}init.sh${NC} included in ${GREEN}.bashrc${NC}"
+    echo -ne "\t${CYAN}init.sh${NC} included in ${GREEN}.bashrc${NC}"
 else
-    echo -e "${YELLOW}init.sh${NC} already included"
+    echo -ne "\t${YELLOW}init.sh${NC} already included"
 fi
+
+sectionlog "symlinks" "end" true
+# ---------------------------------- symlinks ----------------------------------
