6f7fbc0c712493e797210484ed829d0a94fc0b06
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-11-20 19:48:55 +0100

    improved locale setup script stabilitiy

--- scripts/05!_locals.sh
+++ scripts/05!_locals.sh
@@ -1,15 +1,37 @@
 . "$1"
 
+add_locale() {
+    locale_pattern="\s*$1.UTF-8\sUTF-8\s*"
+
+    if ! grep -qE "^$locale_pattern$" /etc/locale.gen; then
+        if grep -qE "^\s*#$locale_pattern$" /etc/locale.gen; then
+            throwiferr sudo sed -i "/^#\s*$locale_pattern/s/^#\s*//" /etc/locale.gen
+        else
+            throwiferr printf "$1 UTF-8\n" | sudo tee -a /etc/locale.gen > /dev/null
+        fi
+    fi
+}
+
 printf "setting up...\n"
 
+if [ ! -f /etc/locale.gen ]; then
+    sudo touch /etc/locale.gen
+fi
+
+case "$OS" in
+    arch|endeavouros)
+        if grep -qE "^NoExtract\s*=\s*usr/share/locale" /etc/pacman.conf; then
+            throwiferr sudo sed -i "/^NoExtract\s*=\s*usr\/share\/locale/s/^/#/" /etc/pacman.conf
+            install_package "glibc"
+        fi
+        ;;
+esac
+
 printf "    ==> adding en_US.UTF-8 locale\n"
-throwiferr sudo sed -i '/^#\s*en_US\.UTF-8\sUTF-8/s/^#\s*//' /etc/locale.gen
+add_locale "en_US"
 
 printf "    ==> adding de_AT.UTF-8 locale\n"
-throwiferr sudo sed -i '/^#\s*de_AT\.UTF-8\sUTF-8/s/^#\s*//' /etc/locale.gen
-
-printf "    ==> generating locales\n"
-throwiferr sudo locale-gen de_AT.UTF-8 en_US.UTF-8 > /dev/null
+add_locale "de_AT"
 
 printf "    ==> setting locale settings\n"
 throwiferr sudo tee /etc/locale.conf > /dev/null << EOF
@@ -25,3 +47,6 @@ LC_ADDRESS=de_AT.UTF-8
 LC_TELEPHONE=de_AT.UTF-8
 LC_IDENTIFICATION=de_AT.UTF-8
 EOF
+
+printf "    ==> generating locales\n"
+throwiferr sudo locale-gen > /dev/null
