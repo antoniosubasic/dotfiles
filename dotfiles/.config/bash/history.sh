91ca1728c64d94b72d9609e0bd08e6f423df1342
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-11-20 12:40:35 +0100

    added custom bash history configuration

---
+++ dotfiles/.config/bash/history.sh
@@ -0,0 +1,5 @@
+export HISTSIZE=10000
+export HISTFILESIZE=10000
+export HISTCONTROL=ignoredups:erasedups
+export HISTIGNORE="history:history *:ls:ll:cd:cd -:pwd:exit:\:q:clear"
+shopt -s histappend
