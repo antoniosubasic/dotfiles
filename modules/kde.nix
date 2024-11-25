f038c1dff9c2ca3020535db588babfaa63c2150d
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-11-25 20:02:41 +0100

    added plasma-browser-integration as kde package

--- modules/kde.nix
+++ modules/kde.nix
@@ -5,7 +5,6 @@
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    plasma-browser-integration
     elisa
     gwenview
     okular
