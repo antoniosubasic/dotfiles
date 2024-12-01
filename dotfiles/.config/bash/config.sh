23a8d3d8e2284734e3e810eb4d2f6fe6b319664a
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-12-01 19:01:22 +0100

    updated .. aliases to include multi-level directory navigation

--- dotfiles/.config/bash/config.sh
+++ dotfiles/.config/bash/config.sh
@@ -23,7 +23,10 @@ alias paste='xsel --output --clipboard'
 
 # cd
 alias ..='cd ..'
-alias ...='cd ../..'
+alias 2..='cd ../..'
+alias 3..='cd ../../..'
+alias 4..='cd ../../../..'
+alias 5..='cd ../../../..'
 
 # asciidoctor
 alias adoc='asciidoctor'
