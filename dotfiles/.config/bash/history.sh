a31a6f2a0eb6c2a686f0dd2c39ffb289f0aa5b26
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-12-06 10:17:36 +0100

    reduce bash history size limits to improve reverse-search results

--- dotfiles/.config/bash/history.sh
+++ dotfiles/.config/bash/history.sh
@@ -1,5 +1,5 @@
-export HISTSIZE=10000
-export HISTFILESIZE=10000
+export HISTSIZE=500
+export HISTFILESIZE=500
 export HISTCONTROL=ignoredups:erasedups
 export HISTIGNORE="history:history *:ls:ll:cd:cd -:pwd:exit:\:q:clear"
 shopt -s histappend
