e8c2df273140248e04b4ae3570ad3ee76427892b
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-12-11 17:25:09 +0100

    added basic flake.nix with home-manager + package installations

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      test-laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./machines/test-laptop/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.antonio = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
