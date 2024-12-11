b04fdbbfc81ba8219b87f77cd5745308ad64e03f
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-12-11 16:04:51 +0100

    initial commit for nixos

--- dotfiles/.config/bash/history.sh
+++
@@ -1,5 +0,0 @@
-export HISTSIZE=500
-export HISTFILESIZE=500
-export HISTCONTROL=ignoredups:erasedups
-export HISTIGNORE="history:history *:ls:ll:cd:cd -:pwd:exit:\:q:clear"
-shopt -s histappend
--- dotfiles/.vimrc
+++
@@ -1,7 +0,0 @@
-syntax on
-
-set ttyfast
-set timeoutlen=10
-let &t_SI = "\e[6 q"
-let &t_SR = "\e[2 q"
-let &t_EI = "\e[2 q"
