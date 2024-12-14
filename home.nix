3994e7d0c2be46506b9adff08f8ca097389a201d
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-12-14 15:16:54 +0100

    added configuration for bat

--- home.nix
+++ home.nix
@@ -19,7 +19,6 @@
       sl
       fzf
       eza
-      bat
       fastfetch
 
       # applications
@@ -181,6 +180,23 @@
   programs.zoxide = {
     enable = true;
     enableBashIntegration = true;
+  };
+
+  programs.bat = {
+    enable = true;
+    themes = {
+      tokyonight = {
+        # use nix-prefetch-git to get necessary info
+        src = pkgs.fetchFromGitHub {
+          owner = "folke";
+          repo = "tokyonight.nvim";
+          rev = "b262293ef481b0d1f7a14c708ea7ca649672e200";
+          sha256 = "1cd8wxgicfm5f6g7lzqfhr1ip7cca5h11j190kx0w52h0kbf9k54";
+        };
+        file = "extras/sublime/tokyonight_night.tmTheme";
+      };
+    };
+    extraPackages = with pkgs.bat-extras; [ batgrep batman prettybat ];
   };
 
 
