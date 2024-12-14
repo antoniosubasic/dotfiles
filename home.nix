21ec5aa8190047db2cf9cfaf65ae3ad6849476a8
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-12-14 11:45:59 +0100

    added git configuration via home.nix

--- home.nix
+++ home.nix
@@ -10,7 +10,6 @@
       man
       curl
       wget
-      git
       unzip
       jq
       xsel
@@ -47,6 +46,34 @@
       # fonts
       jetbrains-mono
     ];
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Antonio Subašić";
+    userEmail = "antonio.subasic.public@gmail.com";
+    aliases = {
+      cm = "commit -m";
+      cp = "!f() { git clone git@github.com:$(git config user.github)/$1.git \${2:-$1}; }; f";
+      transpose-ssh = "!f() { git remote set-url origin $(git remote get-url origin | sed 's|https://github.com/|git@github.com:|'); }; f";
+      transpose-https = "!f() { git remote set-url origin $(git remote get-url origin | sed 's|git@github.com:|https://github.com/|'); }; f";
+      tree = "log --graph --oneline --decorate --all";
+      url = "!f() { git remote get-url origin | sed 's|git@github.com:|https://github.com/|'; }; f";
+    };
+    extraConfig = {
+      user.github = "antoniosubasic";
+      help.autocorrect = 10;
+      url."git@github.com".insteadOf = "gh:";
+      diff.algorithm = "histogram";
+      status.submoduleSummary = true;
+      core = { autocrlf = false; editor = "nvim"; };
+      log.date = "iso";
+      pull.ff = "only";
+      push.autoSetupRemote = true;
+      transfer.fsckobjects = true;
+      fetch.fsckobjects = true;
+      receive.fsckObjects = true;
+    };
   };
 
   programs.neovim = {
