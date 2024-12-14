d72a844969e0009ff88ce8d8f02cb807ec3e47be
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-12-14 12:34:53 +0100

    added alacritty configuration

--- home.nix
+++ home.nix
@@ -26,7 +26,6 @@
       google-chrome
       dropbox
       vscode
-      alacritty
       jetbrains.idea-ultimate
       jetbrains.datagrip
       libreoffice-still
@@ -85,6 +84,68 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env.TERM = "xterm-256color";
+      window = {
+        dimensions = { columns = 125; lines = 30; };
+        padding = { x = 10; y = 10; };
+        decorations = "full";
+        opacity = 0.99;
+      };
+      scrolling.history = 10000;
+      selection.save_to_clipboard = true;
+      font = {
+        normal = {
+          family = "JetBrains Mono";
+          style = "Regular";
+        };
+        bold = {
+          family = "JetBrains Mono";
+          style = "Bold";
+        };
+        italic = {
+          family = "JetBrains Mono";
+          style = "Italic";
+        };
+        bold_italic = {
+          family = "JetBrains Mono";
+          style = "Bold Italic";
+        };
+      };
+      keyboard.bindings = [
+        { key = "N"; mods = "Control"; action = "SpawnNewInstance"; }
+      ];
+      colors = {
+        primary = {
+          background = "#1a1b26";
+          foreground = "#a9b1d6";
+        };
+        normal = {
+          black = "#32344a";
+          red = "#f7768e";
+          green = "#9ece6a";
+          yellow = "#e0af68";
+          blue = "#7aa2f7";
+          magenta = "#ad8ee6";
+          cyan = "#449dab";
+          white = "#787c99";
+        };
+        bright = {
+          black = "#444b6a";
+          red = "#ff7a93";
+          green = "#b9f27c";
+          yellow = "#ff9e64";
+          blue = "#7da6ff";
+          magenta = "#bb9af7";
+          cyan = "#0db9d7";
+          white = "#acb0d0";
+        };
+      };
+    };
   };
 
   programs.bash = {
