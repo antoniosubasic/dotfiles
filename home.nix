6d9b5540ac315c015d6b02e7a72fac8d93fd5a6f
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-12-14 15:33:08 +0100

    added fastfetch configuration

--- home.nix
+++ home.nix
@@ -19,7 +19,6 @@
       sl
       fzf
       eza
-      fastfetch
 
       # applications
       google-chrome
@@ -197,6 +196,41 @@
       };
     };
     extraPackages = with pkgs.bat-extras; [ batgrep batman prettybat ];
+  };
+
+  programs.fastfetch = {
+    enable = true;
+    settings = {
+      modules = [
+        "title"
+        "separator"
+        "os"
+        {
+          "type" = "host";
+          "format" = "{/2}{-}{/}{2}{?3} {3}{?}";
+        }
+        "kernel"
+        "uptime"
+        {
+          "type" = "battery";
+          "format" = "{/4}{-}{/}{4}{?5} [{5}]{?}";
+        }
+        "break"
+        "packages"
+        "shell"
+        "display"
+        "terminal"
+        "break"
+        "cpu"
+        {
+          "type" = "gpu";
+          "key" = "GPU";
+        }
+        "memory"
+        "break"
+        "colors"
+      ];
+    };
   };
 
 
