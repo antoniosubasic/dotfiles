c4537db42fdabf006117d684c8f5cdd6fc957af4
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-12-22 15:53:18 +0100

    configure SDDM to enable auto numlock and set custom lockscreen wallpaper

--- base.nix
+++ base.nix
@@ -57,7 +57,10 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    autoNumlock = true;
+  };
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
