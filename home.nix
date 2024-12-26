c098dc62d2269e5d656e6ae9e41588a2a8ef4335
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-12-26 10:13:47 +0100

    moved nvim configuration files into home.nix

--- home.nix
+++ home.nix
@@ -44,10 +44,6 @@
       # fonts
       (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     ];
-
-    file = {
-      ".config/nvim".source = ./dotfiles/nvim;
-    };
   };
 
   fonts.fontconfig.enable = true;
@@ -85,6 +81,86 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+
+    plugins = with pkgs.vimPlugins; [
+      # core
+      plenary-nvim
+      nvim-web-devicons
+
+      # UI
+      alpha-nvim
+      lualine-nvim
+      tokyonight-nvim
+
+      # functionality
+      telescope-nvim
+      telescope-ui-select-nvim
+      nvim-treesitter
+    ];
+
+    extraLuaConfig = ''
+      vim.g.mapleader = " "
+      vim.g.maplocalleader = "\\"
+
+      vim.opt.expandtab = true
+      vim.opt.tabstop = 4
+      vim.opt.softtabstop = 4
+      vim.opt.shiftwidth = 4
+      vim.opt.number = true
+      vim.opt.relativenumber = true
+
+      vim.cmd.colorscheme("tokyonight-night")
+
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+          "bash", "c", "c_sharp", "cpp", "css", "dockerfile",
+          "go", "html", "java", "javascript", "json", "lua",
+          "markdown", "python", "regex", "rust", "sql",
+          "toml", "typescript", "vue", "xml", "yaml"
+        },
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+
+      local telescope = require('telescope')
+      telescope.setup({
+        pickers = {
+          find_files = { hidden = true },
+        },
+        extensions = {
+          ['ui-select'] = {
+            require('telescope.themes').get_dropdown({})
+          }
+        }
+      })
+      telescope.load_extension('ui-select')
+
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
+
+      require('lualine').setup({
+        options = {
+          theme = 'tokyonight-night'
+        }
+      })
+
+      local alpha = require('alpha')
+      local dashboard = require('alpha.themes.startify')
+
+      dashboard.section.header.val = {
+        [[                                                                        ]],
+        [[           ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗           ]],
+        [[           ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║           ]],
+        [[           ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║           ]],
+        [[           ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║           ]],
+        [[           ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║           ]],
+        [[           ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝           ]],
+      }
+
+      alpha.setup(dashboard.opts)
+    '';
   };
 
   programs.alacritty = {
