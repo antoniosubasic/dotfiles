45bac8f10e475abec8ebc2f374257d33ffeba66c
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-12-27 16:02:01 +0100

    added additional keybindings for zsh

--- home.nix
+++ home.nix
@@ -328,6 +328,11 @@
         bindkey "^T" transpose-chars
         bindkey "^U" backward-kill-line
         bindkey "^W" backward-kill-word
+        bindkey "^[[3~" delete-char
+        bindkey "^[[1;5D" backward-word
+        bindkey "^[[1;5C" forward-word
+        bindkey "^[[3;5~" kill-word
+        bindkey "^H" backward-kill-word
       '';
 
       plugins = [
