59df92e8d33bb51901bd2b04aa80fdcce71f7ac0
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-01-30 14:09:45 +0100

    initial commit for rebuilding kde configuration to use plasma-manager
    removed old configuration + added appearance.nix with wallpaper and
    colorScheme definitions

---
+++ modules/kde/home-configurations/appearance.nix
@@ -0,0 +1,10 @@
+{
+  programs.plasma = {
+    kscreenlocker.appearance.wallpaper = ../../home-configurations/files/images/lockscreen.png;
+    workspace = {
+      wallpaper = ../../home-configurations/files/images/desktop.png;
+      colorScheme = "BreezeDark";
+    };
+    kwin.effects.shakeCursor.enable = false;
+  };
+}
