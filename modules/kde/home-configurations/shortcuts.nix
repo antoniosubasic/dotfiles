213043d7ee2c60b574bb67dce1e47237191c0096
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-03-13 15:47:26 +0100

    added custom keyboard shortcuts

---
+++ modules/kde/home-configurations/shortcuts.nix
@@ -0,0 +1,23 @@
+{
+  programs.plasma.shortcuts = {
+    "services/Alacritty.desktop"."_launch" = "Ctrl+Alt+T";
+    "services/org.kde.kalk.desktop"."_launch" = "Calculator";
+    "services/net.local.printf.desktop"."_launch" = "Ctrl+Alt+S";
+
+    "kwin" = {
+      "Switch One Desktop Down" = "Meta+J";
+      "Switch One Desktop Up" = "Meta+K";
+      "Switch One Desktop to the Left" = "Meta+H";
+      "Switch One Desktop to the Right" = "Meta+L";
+      "Window Maximize" = "Meta+Up";
+      "Window One Desktop Down" = "Meta+Shift+J";
+      "Window One Desktop Up" = "Meta+Shift+K";
+      "Window One Desktop to the Left" = "Meta+Shift+H";
+      "Window One Desktop to the Right" = "Meta+Shift+L";
+    };
+
+    "ksmserver" = {
+      "Lock Session" = "Ctrl+Alt+L";
+    };
+  };
+}
