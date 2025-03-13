6eb7afb6469b83b7dc66fd987d5410ccf9389c6f
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-03-13 17:06:25 +0100

    added power management configuration

---
+++ modules/kde/home-configurations/power.nix
@@ -0,0 +1,50 @@
+{
+  programs.plasma.powerdevil = {
+    AC = {
+      dimDisplay = {
+        enable = true;
+        idleTimeout = 300;
+      };
+      turnOffDisplay.idleTimeout = 330;
+      turnOffDisplay.idleTimeoutWhenLocked = 15;
+      autoSuspend = {
+        action = "sleep";
+        idleTimeout = 420;
+      };
+      powerProfile = "balanced";
+      whenLaptopLidClosed = "sleep";
+      whenSleepingEnter = "standby";
+    };
+    battery = {
+      dimDisplay = {
+        enable = true;
+        idleTimeout = 150;
+      };
+      turnOffDisplay.idleTimeout = 180;
+      turnOffDisplay.idleTimeoutWhenLocked = 15;
+      autoSuspend = {
+        action = "sleep";
+        idleTimeout = 270;
+      };
+      powerProfile = "balanced";
+      whenLaptopLidClosed = "sleep";
+      whenSleepingEnter = "standby";
+    };
+    batteryLevels.lowLevel = 10;
+    lowBattery = {
+      dimDisplay = {
+        enable = true;
+        idleTimeout = 60;
+      };
+      turnOffDisplay.idleTimeout = 90;
+      turnOffDisplay.idleTimeoutWhenLocked = 15;
+      autoSuspend = {
+        action = "sleep";
+        idleTimeout = 150;
+      };
+      powerProfile = "powerSaving";
+      whenLaptopLidClosed = "sleep";
+      whenSleepingEnter = "standby";
+    };
+  };
+}
