d0396994d033120279389f44c098ea37b649b4d8
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-03-14 20:07:38 +0100

    added window rules for discord and vscode

---
+++ modules/kde/home-configurations/windowrules.nix
@@ -0,0 +1,60 @@
+{
+  programs.plasma.window-rules = [
+    {
+      description = "Discord - Virtual Desktop 2";
+      match = {
+        window-class = {
+          value = "discord";
+          type = "exact";
+          match-whole = false;
+        };
+        window-types = [
+          "normal"
+          "desktop"
+          "dock"
+          "toolbar"
+          "torn-of-menu"
+          "dialog"
+          "menubar"
+          "utility"
+          "spash"
+          "osd"
+        ];
+      };
+      apply = {
+        desktops = {
+          value = 2;
+          apply = "force";
+        };
+      };
+    }
+    {
+      description = "VS Code - URL Handler";
+      match = {
+        window-class = {
+          value = "Code";
+          type = "substring";
+          match-whole = true;
+        };
+        window-types = [
+          "normal"
+          "desktop"
+          "dock"
+          "toolbar"
+          "torn-of-menu"
+          "dialog"
+          "menubar"
+          "utility"
+          "spash"
+          "osd"
+        ];
+      };
+      apply = {
+        desktopfile = {
+          value = "/etc/profiles/per-user/antonio/share/applications/code.desktop";
+          apply = "initially";
+        };
+      };
+    }
+  ];
+}
