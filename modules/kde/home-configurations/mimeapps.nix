c1cfae9ebb0bd9fc6afb5394eaa85bb32c00d05d
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-03-14 18:46:32 +0100

    added custom MIME type associations and default applications configuration

---
+++ modules/kde/home-configurations/mimeapps.nix
@@ -0,0 +1,22 @@
+{
+  programs.plasma.configFile."mimeapps.list" = {
+    "Added Associations" = {
+      "application/pdf" = "google-chrome.desktop";
+      "image/png" = "google-chrome.desktop";
+      "image/svg+xml" = "google-chrome.desktop";
+      "text/plain" = "code.desktop";
+      "x-scheme-handler/geo" = "google-maps-geo-handler.desktop";
+    };
+    "Default Applications" = {
+      "application/pdf" = "google-chrome.desktop";
+      "image/png" = "google-chrome.desktop";
+      "image/svg+xml" = "google-chrome.desktop";
+      "text/html" = "google-chrome.desktop";
+      "text/plain" = "code.desktop";
+      "x-scheme-handler/geo" = "google-maps-geo-handler.desktop";
+      "x-scheme-handler/http" = "google-chrome.desktop";
+      "x-scheme-handler/https" = "google-chrome.desktop";
+      "x-scheme-handler/mailto" = "google-chrome.desktop";
+    };
+  };
+}
