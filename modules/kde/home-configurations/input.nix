3537e48d6b0bef743dfbb28c5fbd871aba381732
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-03-14 19:49:17 +0100

    added touchpad and mouse configurations

--- modules/kde/home-configurations/input.nix
+++ modules/kde/home-configurations/input.nix
@@ -1,3 +1,39 @@
 {
-  programs.plasma.input.keyboard.numlockOnStartup = "on";
+  programs.plasma.input = {
+    keyboard.numlockOnStartup = "on";
+    touchpads = [
+      {
+        name = "DLL0A81:00 04F3:314B Touchpad";
+        vendorId = "04F3";
+        productId = "314B";
+        enable = true;
+        disableWhileTyping = true;
+        leftHanded = false;
+        middleButtonEmulation = false;
+        pointerSpeed = 0;
+        scrollSpeed = 0.3;
+        scrollMethod = "twoFingers";
+        naturalScroll = true;
+        tapToClick = true;
+        tapAndDrag = true;
+        tapDragLock = false;
+        twoFingerTap = "rightClick";
+        rightClickMethod = "bottomRight";
+      }
+    ];
+    mice = [
+      {
+        name = "Logitech M705";
+        vendorId = "046D";
+        productId = "406D";
+        enable = true;
+        leftHanded = false;
+        middleButtonEmulation = false;
+        acceleration = 0.2;
+        accelerationProfile = "none";
+        scrollSpeed = 1;
+        naturalScroll = false;
+      }
+    ];
+  };
 }
