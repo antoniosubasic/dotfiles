0c6896d315e6da83849142d7717b3787129b6e8c
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-03-14 18:43:52 +0100

    added advanced power management settings

--- modules/kde/home-configurations/power.nix
+++ modules/kde/home-configurations/power.nix
@@ -1,50 +1,53 @@
 {
-  programs.plasma.powerdevil = {
-    AC = {
-      dimDisplay = {
-        enable = true;
-        idleTimeout = 300;
+  programs.plasma = {
+    configFile."powerdevil.notifyrc"."Event/lowperipheralbattery".Action = "";
+    powerdevil = {
+      AC = {
+        dimDisplay = {
+          enable = true;
+          idleTimeout = 300;
+        };
+        turnOffDisplay.idleTimeout = 330;
+        turnOffDisplay.idleTimeoutWhenLocked = 20;
+        autoSuspend = {
+          action = "sleep";
+          idleTimeout = 420;
+        };
+        powerProfile = "balanced";
+        whenLaptopLidClosed = "sleep";
+        whenSleepingEnter = "standby";
       };
-      turnOffDisplay.idleTimeout = 330;
-      turnOffDisplay.idleTimeoutWhenLocked = 15;
-      autoSuspend = {
-        action = "sleep";
-        idleTimeout = 420;
+      battery = {
+        dimDisplay = {
+          enable = true;
+          idleTimeout = 150;
+        };
+        turnOffDisplay.idleTimeout = 180;
+        turnOffDisplay.idleTimeoutWhenLocked = 20;
+        autoSuspend = {
+          action = "sleep";
+          idleTimeout = 270;
+        };
+        powerProfile = "balanced";
+        whenLaptopLidClosed = "sleep";
+        whenSleepingEnter = "standby";
       };
-      powerProfile = "balanced";
-      whenLaptopLidClosed = "sleep";
-      whenSleepingEnter = "standby";
-    };
-    battery = {
-      dimDisplay = {
-        enable = true;
-        idleTimeout = 150;
+      batteryLevels.lowLevel = 10;
+      lowBattery = {
+        dimDisplay = {
+          enable = true;
+          idleTimeout = 60;
+        };
+        turnOffDisplay.idleTimeout = 90;
+        turnOffDisplay.idleTimeoutWhenLocked = 20;
+        autoSuspend = {
+          action = "sleep";
+          idleTimeout = 150;
+        };
+        powerProfile = "powerSaving";
+        whenLaptopLidClosed = "sleep";
+        whenSleepingEnter = "standby";
       };
-      turnOffDisplay.idleTimeout = 180;
-      turnOffDisplay.idleTimeoutWhenLocked = 15;
-      autoSuspend = {
-        action = "sleep";
-        idleTimeout = 270;
-      };
-      powerProfile = "balanced";
-      whenLaptopLidClosed = "sleep";
-      whenSleepingEnter = "standby";
-    };
-    batteryLevels.lowLevel = 10;
-    lowBattery = {
-      dimDisplay = {
-        enable = true;
-        idleTimeout = 60;
-      };
-      turnOffDisplay.idleTimeout = 90;
-      turnOffDisplay.idleTimeoutWhenLocked = 15;
-      autoSuspend = {
-        action = "sleep";
-        idleTimeout = 150;
-      };
-      powerProfile = "powerSaving";
-      whenLaptopLidClosed = "sleep";
-      whenSleepingEnter = "standby";
     };
   };
 }
