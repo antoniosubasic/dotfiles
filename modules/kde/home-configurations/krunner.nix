fed7a01ff6ebbae4c60f035a0495d379f33d5343
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-03-14 18:42:06 +0100

    added advanced krunner configuration for plugins

--- modules/kde/home-configurations/krunner.nix
+++ modules/kde/home-configurations/krunner.nix
@@ -1,6 +1,12 @@
 {
-  programs.plasma.krunner = {
-    position = "center";
-    historyBehavior = "enableSuggestions";
+  programs.plasma = {
+    krunner = {
+      position = "center";
+      historyBehavior = "enableSuggestions";
+    };
+    configFile.krunnerrc.Plugins = {
+      krunner_konsoleprofilesEnabled = false;
+      Favorites.plugins = "krunner_services,krunner_systemsettings";
+    };
   };
 }
