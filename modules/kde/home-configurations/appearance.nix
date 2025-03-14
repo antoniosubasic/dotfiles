a0707800db3c39e82244b4aa0b9a726e57c8e190
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-03-14 18:34:45 +0100

    added advanced kwin configuration for hot corners

--- modules/kde/home-configurations/appearance.nix
+++ modules/kde/home-configurations/appearance.nix
@@ -6,5 +6,9 @@
       colorScheme = "BreezeDark";
     };
     kwin.effects.shakeCursor.enable = false;
+    configFile.kwinrc = {
+      Effect-overview.BorderActivate = 9;
+      Xwayland.Scale = "1.85";
+    };
   };
 }
