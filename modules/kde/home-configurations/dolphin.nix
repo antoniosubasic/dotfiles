a5e7d276a0838dedc0e5d40ffd3058b22466325c
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-03-14 18:51:45 +0100

    added advanced configuration for trash in dolphin file manager

--- modules/kde/home-configurations/dolphin.nix
+++ modules/kde/home-configurations/dolphin.nix
@@ -1,21 +1,34 @@
+{ username, ... }:
+
 {
-  programs.plasma.configFile.dolphinrc = {
-    DetailsMode = {
-      ExpandableFolders = false;
-      PreviewSize = 22;
+  programs.plasma.configFile = {
+    dolphinrc = {
+      DetailsMode = {
+        ExpandableFolders = false;
+        PreviewSize = 22;
+      };
+      General = {
+        ConfirmClosingMultipleTabs = false;
+        OpenExternallyCalledFolderInNewTab = true;
+        RememberOpenedTabs = false;
+        ShowFullPath = true;
+        SortingChoice = "CaseInsensitiveSorting";
+      };
+      MainWindow = {
+        MenuBar = "Disabled";
+        ToolBarsMovable = "Disabled";
+      };
+      PreviewSettings.Plugins = "appimagethumbnail,audiothumbnail,comicbookthumbnail,cursorthumbnail,djvuthumbnail,ebookthumbnail,exrthumbnail,directorythumbnail,fontthumbnail,imagethumbnail,jpegthumbnail,jxl,kraorathumbnail,windowsexethumbnail,windowsimagethumbnail,opendocumentthumbnail,svgthumbnail,gdk-pixbuf-thumbnailer";
+      Search.Location = "Everywhere";
     };
-    General = {
-      ConfirmClosingMultipleTabs = false;
-      OpenExternallyCalledFolderInNewTab = true;
-      RememberOpenedTabs = false;
-      ShowFullPath = true;
-      SortingChoice = "CaseInsensitiveSorting";
+    ktrashrc = {
+      "/home/${username}/.local/share/Trash" = {
+        Days = 100;
+        LimitReachedAction = 0;
+        Percent = 10;
+        UseSizeLimit = true;
+        UseTimeLimit = true;
+      };
     };
-    MainWindow = {
-      MenuBar = "Disabled";
-      ToolBarsMovable = "Disabled";
-    };
-    PreviewSettings.Plugins = "appimagethumbnail,audiothumbnail,comicbookthumbnail,cursorthumbnail,djvuthumbnail,ebookthumbnail,exrthumbnail,directorythumbnail,fontthumbnail,imagethumbnail,jpegthumbnail,jxl,kraorathumbnail,windowsexethumbnail,windowsimagethumbnail,opendocumentthumbnail,svgthumbnail,gdk-pixbuf-thumbnailer";
-    Search.Location = "Everywhere";
   };
 }
