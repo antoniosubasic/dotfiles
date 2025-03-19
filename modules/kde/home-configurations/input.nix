1f50dc15f8fd318bc223a827ab6b6cbbf6a1906c
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-03-19 20:22:38 +0100

    refactored input configurations
    touchpads missing 'PointerAccelerationProfile' attribute - added code to
    set this attribute manually

--- modules/kde/home-configurations/input.nix
+++ modules/kde/home-configurations/input.nix
@@ -1,39 +1,64 @@
+{ lib, ... }:
+
+let
+  touchpads = [
+    {
+      name = "DLL0A81:00 04F3:314B Touchpad";
+      vendorId = "04F3";
+      productId = "314B";
+    }
+  ];
+
+  mice = [
+    {
+      name = "Logitech M705";
+      vendorId = "046D";
+      productId = "406D";
+      enable = true;
+      leftHanded = false;
+      middleButtonEmulation = false;
+      acceleration = 0.2;
+      accelerationProfile = "none";
+      scrollSpeed = 1;
+      naturalScroll = false;
+    }
+  ];
+in
 {
-  programs.plasma.input = {
-    keyboard.numlockOnStartup = "on";
-    touchpads = [
-      {
-        name = "DLL0A81:00 04F3:314B Touchpad";
-        vendorId = "04F3";
-        productId = "314B";
-        enable = true;
-        disableWhileTyping = true;
-        leftHanded = false;
-        middleButtonEmulation = false;
-        pointerSpeed = 0;
-        scrollSpeed = 0.3;
-        scrollMethod = "twoFingers";
-        naturalScroll = true;
-        tapToClick = true;
-        tapAndDrag = true;
-        tapDragLock = false;
-        twoFingerTap = "rightClick";
-        rightClickMethod = "bottomRight";
-      }
-    ];
-    mice = [
-      {
-        name = "Logitech M705";
-        vendorId = "046D";
-        productId = "406D";
-        enable = true;
-        leftHanded = false;
-        middleButtonEmulation = false;
-        acceleration = 0.2;
-        accelerationProfile = "none";
-        scrollSpeed = 1;
-        naturalScroll = false;
-      }
-    ];
+  programs.plasma = {
+    input = {
+      keyboard.numlockOnStartup = "on";
+      touchpads = map (
+        identifier:
+        identifier
+        // {
+          enable = true;
+          disableWhileTyping = true;
+          leftHanded = false;
+          middleButtonEmulation = false;
+          pointerSpeed = 0;
+          scrollSpeed = 0.3;
+          scrollMethod = "twoFingers";
+          naturalScroll = true;
+          tapToClick = true;
+          tapAndDrag = true;
+          tapDragLock = false;
+          twoFingerTap = "rightClick";
+          rightClickMethod = "bottomRight";
+        }
+      ) touchpads;
+      mice = mice;
+    };
+
+    configFile.kcminputrc = builtins.listToAttrs (
+      map (touchpad: {
+        name = "Libinput/${builtins.toString (lib.fromHexString touchpad.vendorId)}/${builtins.toString (lib.fromHexString touchpad.productId)}/${
+          lib.escape [ "/" ] touchpad.name
+        }";
+        value = {
+          PointerAccelerationProfile = 1;
+        };
+      }) touchpads
+    );
   };
 }
