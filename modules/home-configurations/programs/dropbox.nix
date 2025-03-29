2e62381f64a44eec9583133bbf787cfbd6c50583
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-03-29 12:30:01 +0100

    added dropbox autostart configuration

--- modules/home-configurations/programs/dropbox.nix
+++ modules/home-configurations/programs/dropbox.nix
@@ -1,3 +1,16 @@
 {
   services.dropbox.enable = true;
+  home.file.".config/autostart/dropbox.desktop".text = ''
+    [Desktop Entry]
+    Name=Dropbox
+    GenericName=File Synchronizer
+    Comment=Sync your files across computers and to the we
+    Exec=dropbox start -i
+    Terminal=false
+    Type=Application
+    Icon=dropbox
+    Categories=Network;FileTransfer;
+    Keywords=file;synchronization;sharing;collaboration;cloud;storage;backup;
+    StartupNotify=false
+  '';
 }
