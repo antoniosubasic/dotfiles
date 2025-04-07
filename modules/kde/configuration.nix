9e0e123e9fb0654e9e89805b44c4d698d5b6ba70
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-04-07 13:13:38 +0200

    fix(appearance): removed system wide appearance configuration
    problem fixed: symlinked images are being garbage collected

--- modules/kde/configuration.nix
+++ modules/kde/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, username, ... }:
+{ pkgs, ... }:
 
 {
   services = {
@@ -13,24 +13,6 @@
       gwenview
       okular
       khelpcenter
-    ];
-
-    systemPackages = [
-      (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
-        [General]
-        background=${./home-configurations/appearance/lockscreen.png}
-      '')
-
-      # also see ./home-configurations/appearance.nix
-      (pkgs.symlinkJoin {
-        name = "sddm-face-icon";
-        paths = [
-          (pkgs.runCommand "sddm-face-icon" { } ''
-            mkdir -p $out/share/sddm/faces
-            cp ${./home-configurations/appearance/avatar.png} $out/share/sddm/faces/${username}.face.icon
-          '')
-        ];
-      })
     ];
   };
 }
--- modules/kde/home-configurations/appearance.nix
+++ modules/kde/home-configurations/appearance.nix
@@ -1,14 +1,9 @@
 {
   programs.plasma = {
-    kscreenlocker.appearance.wallpaper = ./appearance/lockscreen.png;
-    workspace = {
-      wallpaper = ./appearance/desktop.png;
-      colorScheme = "BreezeDark";
-    };
+    workspace.colorScheme = "BreezeDark";
     kwin.effects.shakeCursor.enable = false;
     configFile.kwinrc.Effect-overview.BorderActivate = 9;
   };
 
-  # also see ../configuration.nix
   home.file.".face.icon".source = ./appearance/avatar.png;
 }
