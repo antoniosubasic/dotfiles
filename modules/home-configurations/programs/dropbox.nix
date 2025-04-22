685bdda38beaa88aecc866fbb4eefafe764660c7
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-04-22 13:33:50 +0200

    refactor(dropbox, keepassxc): remove custom autostart configurations

--- modules/home-configurations/programs/dropbox.nix
+++ modules/home-configurations/programs/dropbox.nix
@@ -7,17 +7,4 @@ lib.optionalAttrs
   ])
   {
     services.dropbox.enable = true;
-    home.file.".config/autostart/dropbox.desktop".text = ''
-      [Desktop Entry]
-      Name=Dropbox
-      GenericName=File Synchronizer
-      Comment=Sync your files across computers and to the we
-      Exec=dropbox start -i
-      Terminal=false
-      Type=Application
-      Icon=dropbox
-      Categories=Network;FileTransfer;
-      Keywords=file;synchronization;sharing;collaboration;cloud;storage;backup;
-      StartupNotify=false
-    '';
   }
--- modules/home-configurations/programs/keepassxc.nix
+++ modules/home-configurations/programs/keepassxc.nix
@@ -11,22 +11,5 @@ lib.optionalAttrs
     "personal"
   ])
   {
-    home = {
-      packages = [ pkgs.keepassxc ];
-      file = {
-        ".config/autostart/keepassxc.desktop".text = ''
-          [Desktop Entry]
-          Name=KeePassXC
-          GenericName=Password Manager
-          Comment=Manage your passwords
-          Exec=keepassxc --minimized
-          Terminal=false
-          Type=Application
-          Icon=keepassxc
-          Categories=Security;PasswordManager;
-          Keywords=security;password;manager;keepassxc;
-          StartupNotify=false
-        '';
-      };
-    };
+    home.packages = [ pkgs.keepassxc ];
   }
