a24699dabdb76cd942c09199087a2f9617940590
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-04-22 10:30:06 +0200

    refactor(templates): remove templates.nix + template files

--- modules/home-configurations/templates.nix
+++
@@ -1,8 +0,0 @@
-{ lib, utilities, ... }:
-
-lib.optionalAttrs (utilities.hasTag "personal") {
-  home.activation.copyTemplates = lib.mkAfter ''
-    rm -rf $HOME/Templates/*
-    cp -r ${./files/templates}/* $HOME/Templates/
-  '';
-}
