baccc55ac8be26cd9bd9f08e39e8ac853ccbc9c6
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-05-01 10:15:41 +0200

    feat(hyprland): add very basic example hypr.conf configuration

--- modules/hyprland/home.nix
+++ modules/hyprland/home.nix
@@ -1,5 +1,61 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
+    settings = {
+      "$mod" = "SUPER";
+      "$terminal" = "alacritty";
+      "$menu" = "rofi -show drun";
+      "$fileManager" = "dolphin";
+
+      decoration = {
+        shadow_offset = "0 5";
+        "col.shadow" = "rgba(00000099)";
+      };
+
+      input = {
+        kb_layout = "at";
+      };
+
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+        "$mod ALT, mouse:272, resizewindow"
+
+        "$mod, Q, exec, $terminal"
+        "$mod, C, exec, killactive"
+        "$mod, E, exec, $fileManager"
+        "$mod, F, exec, $menu"
+
+        "$mod, h, movefocus, l"
+        "$mod, j, movefocus, d"
+        "$mod, k, movefocus, u"
+        "$mod, l, movefocus, r"
+        
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
+
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 10"
+
+        "$mod ALT, l, workspace, e+1"
+        "$mod ALT, h, workspace, e-1"
+      ];
+    };
   };
 }
