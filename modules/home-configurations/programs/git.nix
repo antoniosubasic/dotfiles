c5a589f30385ff12511ca157928f7db53234f78f
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-06-12 12:36:38 +0200

    feat(git): add alias for commit amend

--- modules/home-configurations/programs/git.nix
+++ modules/home-configurations/programs/git.nix
@@ -7,6 +7,7 @@
     userEmail = "antonio.subasic.public@gmail.com";
     aliases = {
       cm = "commit -m";
+      ca = "commit --amend";
       cp = "!f() { git clone git@github.com:$(git config user.github)/$1.git \${2:-$1}; }; f";
       transpose-ssh = "!f() { git remote set-url origin $(git remote get-url origin | sed 's|https://github.com/|git@github.com:|'); }; f";
       transpose-https = "!f() { git remote set-url origin $(git remote get-url origin | sed -e 's|git@github.com:|https://github.com/|' -e 's|\.git$||'); }; f";
