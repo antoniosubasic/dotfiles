511f00552b20243bcfb47e7f142f850099e7b362
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-06-16 21:20:51 +0200

    refactor(mimeapps): unify applications into a single set

--- modules/kde/home-configurations/mimeapps.nix
+++ modules/kde/home-configurations/mimeapps.nix
@@ -5,40 +5,29 @@ let
   plaintext = "org.kde.kate.desktop";
   browser = "google-chrome.desktop";
   geolocation = "google-maps-geo-handler.desktop";
+
+  set = {
+    "application/pdf" = pdf;
+    "image/avif" = image;
+    "image/bmp" = image;
+    "image/heif" = image;
+    "image/jpeg" = image;
+    "image/png" = image;
+    "image/svg+xml" = image;
+    "image/webp" = image;
+    "image/x-icns" = image;
+    "video/mp4" = video;
+    "text/html" = browser;
+    "text/plain" = plaintext;
+    "x-scheme-handler/geo" = geolocation;
+    "x-scheme-handler/http" = browser;
+    "x-scheme-handler/https" = browser;
+    "x-scheme-handler/mailto" = browser;
+  };
 in
 {
   programs.plasma.configFile."mimeapps.list" = {
-    "Added Associations" = {
-      "application/pdf" = pdf;
-      "image/avif" = image;
-      "image/bmp" = image;
-      "image/heif" = image;
-      "image/jpeg" = image;
-      "image/png" = image;
-      "image/svg+xml" = image;
-      "image/webp" = image;
-      "image/x-icns" = image;
-      "video/mp4" = video;
-      "text/plain" = plaintext;
-      "x-scheme-handler/geo" = geolocation;
-    };
-    "Default Applications" = {
-      "application/pdf" = pdf;
-      "image/avif" = image;
-      "image/bmp" = image;
-      "image/heif" = image;
-      "image/jpeg" = image;
-      "image/png" = image;
-      "image/svg+xml" = image;
-      "image/webp" = image;
-      "image/x-icns" = image;
-      "video/mp4" = video;
-      "text/html" = browser;
-      "text/plain" = plaintext;
-      "x-scheme-handler/geo" = geolocation;
-      "x-scheme-handler/http" = browser;
-      "x-scheme-handler/https" = browser;
-      "x-scheme-handler/mailto" = browser;
-    };
+    "Added Associations" = set;
+    "Default Applications" = set;
   };
 }
