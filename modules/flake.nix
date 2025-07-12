bb42d8afa9138ae63e1d46d703999ca68b4f8b76
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-07-12 16:04:09 +0200

    refactor(modules/flake): dynamically import directories for custom modules

--- modules/flake.nix
+++ modules/flake.nix
@@ -3,11 +3,13 @@
 
   outputs =
     { ... }:
+    let
+      contents = builtins.readDir ./.;
+      directories = builtins.filter (name: contents.${name} == "directory") (builtins.attrNames contents);
+    in
     {
       nixosModules.customModules = {
-        imports = [
-          ./sqldeveloper
-        ];
+        imports = map (dir: ./${dir}) directories;
       };
     };
 }
