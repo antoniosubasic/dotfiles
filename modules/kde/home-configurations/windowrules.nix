5f041845c9517c1ecb94d7dc290e578ce01d058d
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-09-18 19:13:57 +0200

    style(windowrules): format code

--- modules/kde/home-configurations/windowrules.nix
+++ modules/kde/home-configurations/windowrules.nix
@@ -1,127 +1,127 @@
 { lib, utilities, ... }:
 
 {
-  programs.plasma.window-rules =
-    [
-      {
-        description = "VS Code - URL Handler";
-        match = {
-          window-class = {
-            value = "Code";
-            type = "substring";
-            match-whole = true;
-          };
-          window-types = [
-            "normal"
-            "desktop"
-            "dock"
-            "toolbar"
-            "torn-of-menu"
-            "dialog"
-            "menubar"
-            "utility"
-            "spash"
-            "osd"
-          ];
+  programs.plasma.window-rules = [
+    {
+      description = "VS Code - URL Handler";
+      match = {
+        window-class = {
+          value = "Code";
+          type = "substring";
+          match-whole = true;
         };
-        apply = {
-          desktopfile = {
-            value = "/etc/profiles/per-user/antonio/share/applications/code.desktop";
-            apply = "initially";
-          };
+        window-types = [
+          "normal"
+          "desktop"
+          "dock"
+          "toolbar"
+          "torn-of-menu"
+          "dialog"
+          "menubar"
+          "utility"
+          "spash"
+          "osd"
+        ];
+      };
+      apply = {
+        desktopfile = {
+          value = "/etc/profiles/per-user/antonio/share/applications/code.desktop";
+          apply = "initially";
         };
-      }
-      {
-        description = "Fullscreen";
-        match = {
-          window-class = {
-            value = "Google Chrome|Code";
-            type = "regex";
-            match-whole = true;
-          };
-          window-types = [
-            "normal"
-            "desktop"
-            "dock"
-            "toolbar"
-            "torn-of-menu"
-            "dialog"
-            "menubar"
-            "utility"
-            "spash"
-            "osd"
-          ];
+      };
+    }
+    {
+      description = "Fullscreen";
+      match = {
+        window-class = {
+          value = "Google Chrome|Code";
+          type = "regex";
+          match-whole = true;
         };
-        apply = rec {
-          maximizehoriz = {
-            value = true;
-            apply = "initially";
-          };
-          maximizevert = maximizehoriz;
+        window-types = [
+          "normal"
+          "desktop"
+          "dock"
+          "toolbar"
+          "torn-of-menu"
+          "dialog"
+          "menubar"
+          "utility"
+          "spash"
+          "osd"
+        ];
+      };
+      apply = rec {
+        maximizehoriz = {
+          value = true;
+          apply = "initially";
         };
-      }
-    ]
-    ++ lib.optionals (utilities.hasTag "virtual_desktops") [
-      {
-        description = "Discord - Virtual Desktop 2";
-        match = {
-          window-class = {
-            value = "discord";
-            type = "exact";
-            match-whole = false;
-          };
-          window-types = [
-            "normal"
-            "desktop"
-            "dock"
-            "toolbar"
-            "torn-of-menu"
-            "dialog"
-            "menubar"
-            "utility"
-            "spash"
-            "osd"
-          ];
+        maximizevert = maximizehoriz;
+      };
+    }
+  ]
+  ++ lib.optionals (utilities.hasTag "virtual_desktops") [
+    {
+      description = "Discord - Virtual Desktop 2";
+      match = {
+        window-class = {
+          value = "discord";
+          type = "exact";
+          match-whole = false;
         };
-        apply = {
-          desktops = {
-            value = "Desktop_2";
-            apply = "initially";
-          };
+        window-types = [
+          "normal"
+          "desktop"
+          "dock"
+          "toolbar"
+          "torn-of-menu"
+          "dialog"
+          "menubar"
+          "utility"
+          "spash"
+          "osd"
+        ];
+      };
+      apply = {
+        desktops = {
+          value = "Desktop_2";
+          apply = "initially";
         };
-      }
-    ] ++ lib.optionals (!(utilities.hasTag "virtual_desktops")) [
-      {
-        description = "Discord - Desktop Monitor 2";
-        match = {
-          window-class = {
-            value = "discord";
-            type = "exact";
-            match-whole = false;
-          };
-          window-types = [
-            "normal"
-            "desktop"
-            "dock"
-            "toolbar"
-            "torn-of-menu"
-            "dialog"
-            "menubar"
-            "utility"
-            "spash"
-            "osd"
-          ];
+      };
+    }
+  ]
+  ++ lib.optionals (!(utilities.hasTag "virtual_desktops")) [
+    {
+      description = "Discord - Desktop Monitor 2";
+      match = {
+        window-class = {
+          value = "discord";
+          type = "exact";
+          match-whole = false;
         };
-        apply = {
-          screen = {
-            value = 0;
-            apply = "initially";
-          };
-          ignoregeometry = {
-            value = true;
-            apply = "initially";
-          };
+        window-types = [
+          "normal"
+          "desktop"
+          "dock"
+          "toolbar"
+          "torn-of-menu"
+          "dialog"
+          "menubar"
+          "utility"
+          "spash"
+          "osd"
+        ];
+      };
+      apply = {
+        screen = {
+          value = 0;
+          apply = "initially";
         };
-      }
-    ];
+        ignoregeometry = {
+          value = true;
+          apply = "initially";
+        };
+      };
+    }
+  ];
 }
