87c70663467153a17b75d6c38405a0df22c5bc07
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-09-19 17:07:01 +0200

    feat(appearance): add wallpaper configuration for lockscreen and desktop

--- modules/kde/home-configurations/appearance.nix
+++ modules/kde/home-configurations/appearance.nix
@@ -1,8 +1,18 @@
+let
+  toPath =
+    path:
+    builtins.path {
+      inherit path;
+      name = builtins.baseNameOf (toString path);
+    };
+in
 {
   programs.plasma = {
     workspace.colorScheme = "BreezeDark";
     kwin.effects.shakeCursor.enable = false;
     configFile.kwinrc.Effect-overview.BorderActivate = 9;
+    kscreenlocker.appearance.wallpaper = toPath ./appearance/lockscreen.png;
+    workspace.wallpaper = toPath ./appearance/desktop.png;
   };
 
   home.file.".face.icon".source = ./appearance/avatar.png;
