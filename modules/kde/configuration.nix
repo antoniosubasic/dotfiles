4da72f5c57075bf5328f8ef13bb320d1ffc32bc3
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-09-20 10:13:02 +0200

    feat(kde/sddm): add lockscreen background configuration

--- modules/kde/configuration.nix
+++ modules/kde/configuration.nix
@@ -1,10 +1,20 @@
-{ utilities, ... }:
+{ pkgs, utilities, ... }:
 
-{
+rec {
   imports = utilities.importNixFiles ./configurations;
 
   services = {
-    displayManager.sddm.enable = true;
+    displayManager.sddm = {
+      enable = true;
+      theme = "breeze";
+    };
     desktopManager.plasma6.enable = true;
   };
+
+  environment.systemPackages = [
+    (pkgs.writeTextDir "share/sddm/themes/${services.displayManager.sddm.theme}/theme.conf.user" ''
+      [General]
+      background=${builtins.path { path = ./home-configurations/appearance/lockscreen.png; }}
+    '')
+  ];
 }
