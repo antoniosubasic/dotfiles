f9be8c7907431800603120c9a8860246bbd8bdbf
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-10-12 11:19:43 +0200

    feat(gh): add interactive (fzf) clone script for gh repos

--- modules/home-configurations/programs/gh.nix
+++ modules/home-configurations/programs/gh.nix
@@ -13,6 +13,34 @@
     ];
     extensions = with pkgs; [
       gh-contribs
+
+      (pkgs.writeShellApplication rec {
+        name = "gh-ic";
+        derivationArgs.pname = name;
+        runtimeInputs = with pkgs; [
+          jq
+          fzf
+        ];
+        text = ''
+          #!/usr/bin/env bash
+          selected="$(
+            gh api user/repos --paginate | jq -r '.[].full_name' | \
+            fzf --preview 'GH_FORCE_TTY=1 gh repo view {}' --expect=enter,alt-enter --bind 'alt-enter:accept'
+          )"
+          key=$(echo "$selected" | head -1)
+          repo=$(echo "$selected" | tail -n +2)
+          if [ -n "$repo" ]; then
+            if [ "$key" = 'alt-enter' ]; then
+              read -r -p 'clone as: ' name
+            fi
+            if [ -z "''${name+x}" ] || [ -z "$name" ]; then
+              gh repo clone "$repo"
+            else
+              gh repo clone "$repo" "$name"
+            fi
+          fi
+        '';
+      })
     ];
     gitCredentialHelper.enable = false;
     settings = {
