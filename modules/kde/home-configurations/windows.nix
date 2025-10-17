df389ee6c54fe559d99c1a9e24229c0bb0750e21
Antonio Subašić <antonio.subasic.as@gmail.com>
2025-10-17 21:02:01 +0200

    feat(kde): change virtual desktop navigation to wrap around

--- modules/kde/home-configurations/windows.nix
+++ modules/kde/home-configurations/windows.nix
@@ -16,5 +16,6 @@
     cornerBarrier = true;
     virtualDesktops.number =
       if builtins.hasAttr "virtual_desktops" userVars then userVars.virtual_desktops else 1;
+    effects.desktopSwitching.navigationWrapping = true;
   };
 }
