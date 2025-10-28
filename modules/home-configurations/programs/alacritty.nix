ce6c0d5f7c39b9c99e29c2ed38d99ce41b731484
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-10-28 10:27:09 +0100

    refactor(alacritty): update terminal style configuration
    change cursor to beam instead of block
    update tokyonight colors

--- modules/home-configurations/programs/alacritty.nix
+++ modules/home-configurations/programs/alacritty.nix
@@ -22,6 +22,7 @@
       };
       scrolling.history = 10000;
       selection.save_to_clipboard = true;
+      cursor.style = "Beam";
       font = {
         normal = {
           family = "JetBrainsMono Nerd Font";
@@ -50,27 +51,27 @@
       colors = {
         primary = {
           background = "#1a1b26";
-          foreground = "#a9b1d6";
+          foreground = "#c0caf5";
         };
         normal = {
-          black = "#32344a";
+          black = "#15161e";
           red = "#f7768e";
           green = "#9ece6a";
           yellow = "#e0af68";
           blue = "#7aa2f7";
-          magenta = "#ad8ee6";
-          cyan = "#449dab";
-          white = "#787c99";
+          magenta = "#bb9af7";
+          cyan = "#7dcfff";
+          white = "#a9b1d6";
         };
         bright = {
-          black = "#444b6a";
-          red = "#ff7a93";
-          green = "#b9f27c";
-          yellow = "#ff9e64";
-          blue = "#7da6ff";
+          black = "#414868";
+          red = "#f7768e";
+          green = "#9ece6a";
+          yellow = "#e0af68";
+          blue = "#7aa2f7";
           magenta = "#bb9af7";
-          cyan = "#0db9d7";
-          white = "#acb0d0";
+          cyan = "#7dcfff";
+          white = "#c0caf5";
         };
       };
     };
